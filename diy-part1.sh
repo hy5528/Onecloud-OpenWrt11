d65919df297caf5f771af70107159534fa4a4d16
hy5528 <95360525+hy5528@users.noreply.github.com>
2025-03-07 21:51:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -60,7 +60,7 @@ merge_package dev https://github.com/vernesong/OpenClash package/app luci-app-op
 # 内核，参照 kiddin9
 shopt -s extglob
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-merge_package main https://github.com/lxiaya/openwrt-onecloud target/linux target/linux/amlogic
+merge_package main https://github.com/hy5528/Onecloud-OpenWrt11 target/linux target/linux/amlogic
 sed -i "s/wpad-openssl/wpad-basic-mbedtls/" target/linux/amlogic/image/Makefile
 # sed -i "s/neon-vfpv4/vfpv4/" target/linux/amlogic/meson8b/target.mk
 rm -rf package/feeds/routing/batman-adv
